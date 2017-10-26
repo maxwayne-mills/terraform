41875b8b1a4d9d0f07e696a704c7ba6655ee448b
Clarence Mills <clarence.mills@gmail.com>
2017-10-26 19:02:42 -0400

    Build clarencemills website using terraform

--- clarencemills/web.tf
+++ clarencemills/web.tf
@@ -1,8 +1,32 @@
 resource "aws_instance" "www-1" {
- ami		= "ami-13e45c77"
- instance_type	= "t2.micro"
+  ami		= "ami-13e45c77"
+  instance_type	= "t2.micro"
 
- tags {
-  name		= "w1-clarencemills"
- }   
+  tags {
+   name		= "www-1"
+  }   
+
+  provisioner "local-exec" {
+    command 	= "echo ${aws_instance.www-1.private_ip} >> private_ips.txt",
+  }
+
+  provisioner "local-exec" {
+    command	= "echo ${aws_instance.www-1.public_ip} >> public_ips.txt"
+  }
+
+  provisioner "local-exec" {
+    command 	= "sleep 120; ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook add_users.yml -vvvv" 
+  }
+}
+
+resource "aws_security_group" "web-sg" {
+  name		= "web-instance-security-group"
+
+  ingress {
+    from_port	= 8080
+    to_port	= 8080
+    protocol	= "tcp"
+    cidr_blocks	= ["0.0.0.0/0"]
+  }
+
 }
