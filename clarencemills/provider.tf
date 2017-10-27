23eb017214db1cbf6a7afe52728282dd97876515
Clarence Mills <clarence.mills@gmail.com>
2017-10-26 20:23:38 -0400

    add variable declaration

--- clarencemills/provider.tf
+++ clarencemills/provider.tf
@@ -1,2 +1,3 @@
 provider "aws" {
+  region = "${var.aws_region}"
 }
--- clarencemills/web.tf
+++ clarencemills/web.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "www-1" {
   ami		= "ami-13e45c77"
-  instance_type	= "t2.micro"
+  instance_type	= "${var.web_instance_type}"
 
   tags {
    name		= "www-1"
@@ -15,16 +15,20 @@ resource "aws_instance" "www-1" {
   }
 
   provisioner "local-exec" {
-    command 	= "sleep 120; ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook add_users.yml -vvvv" 
+    command 	= "sleep 120 && ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook config_mgmt/add_users.yml -vvvv" 
   }
+}
+
+resource "aws_eip" "cmils" {
+  instance = "${aws_instance.cmills.id}"
 }
 
 resource "aws_security_group" "web-sg" {
   name		= "web-instance-security-group"
 
   ingress {
-    from_port	= 8080
-    to_port	= 8080
+    from_port	= 80
+    to_port	= 80
     protocol	= "tcp"
     cidr_blocks	= ["0.0.0.0/0"]
   }
