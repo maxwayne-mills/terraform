b4453d2dbe77c943c478739b3010714b0beb374b
Clarence Mills <clarence.mills@gmail.com>
2017-10-27 19:20:27 -0400

    add vpc,subnet, updated securiy_group with subnet id

--- becomeonewiththecode/instance.tf
+++ becomeonewiththecode/instance.tf
@@ -1,5 +1,21 @@
-resource "aws_security_group" "web-sg-1" {
+resource "aws_vpc" "bcode" {
+  cidr_block = "10.1.0.0/16"
+  tags {
+    Name = "become one with the code"
+  }
+}
+
+resource "aws_subnet" "private-1" {
+  vpc_id      = "${aws_vpc.bcode.id}"
+  cidr_block  = "10.1.1.0/24"
+  tags {
+    Name = "private-1"
+  }
+}
+
+resource "aws_security_group" "access_web_sg" {
   name		= "web-instance-security-group"
+  vpc_id  = "${aws_vpc.bcode.id}"
   ingress {
     from_port	= 80
     to_port	= 80
@@ -17,8 +33,8 @@ resource "aws_security_group" "web-sg-1" {
 resource "aws_instance" "www-1" {
   ami = "ami-13e45c77"
   instance_type = "t2.micro"
-  #security_groups = ["web-sg"]
-  security_groups = ["${aws_security_group.web-sg-1.name}"]
+  subnet_id = "${aws_subnet.private-1.id}"
+  security_groups = ["${aws_security_group.access_web_sg.id}"]
   tags {
     Name = "www-1"
   }
