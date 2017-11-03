adf8f50839c624c7e455b65ef32430049425427c
Clarence Mills <clarence.mills@gmail.com>
2017-11-03 09:13:27 -0400

    Changed secrutiy group name to test_instance

--- test/ec2.tf
+++ test/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "test_ec2" {
   ami                    = "${var.ami_name}"
   instance_type          = "${var.instance_name}"
   key_name               = "cmills-key"
-  vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
+  vpc_security_group_ids = ["${aws_security_group.test_instance.id}"]
 
   tags {
     Name = "Test instance"
