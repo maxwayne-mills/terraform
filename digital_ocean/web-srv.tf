6ff0d1078a205d5eaf55f9020a192d5c20003314
Clarence Mills <clarence.mills@gmail.com>
2018-02-17 17:02:50 -0500

    Add: ansible provisioner

--- digital_ocean/web-srv.tf
+++ digital_ocean/web-srv.tf
@@ -4,4 +4,8 @@ resource "digitalocean_droplet" "www1" {
     region = "${var.region}"
     size = "${var.droplet_memory_size}"
     ssh_keys = ["${var.ssh_fingerprint}"]
+
+    provisioner "local-exec" {
+        command = "sleep 20 && echo \"[webserver1]\n${digitalocean_droplet.www1.ipv4_address} ansible_connection=ssh ansible_ssh_user=root\" > inventory && ansible-playbook -i inventory ../../config-managment/roles/users/users.yml"
+    }
 }
