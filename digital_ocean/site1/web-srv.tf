cb166f7e95610f6e5c8c2e439e052861a6b541bb
Clarence Mills <clarence.mills@gmail.com>
2018-02-18 20:36:58 -0500

    Split functional into two, site1 create seperate infrastructure, mail add's key and so forth

---
+++ digital_ocean/site1/web-srv.tf
@@ -0,0 +1,11 @@
+resource "digitalocean_droplet" "www1" {
+    image = "${var.droplet_image}"
+    name = "${var.websrv_name}"
+    region = "${var.region}"
+    size = "${var.droplet_memory_size}"
+    ssh_keys = ["${var.ssh_fingerprint}"]
+
+    provisioner "local-exec" {
+        command = "sleep 20 && echo \"[webserver1]\n${digitalocean_droplet.www1.ipv4_address} ansible_connection=ssh ansible_ssh_user=root\" > inventory && ansible-playbook -i inventory config-managment/roles/websrv/websrv.yml"
+    }
+}
