f76a7023e9c6183bb477ac04112c6447c7519a89
Clarence Mills <clarence.mills@gmail.com>
2018-02-18 21:38:15 -0500

    files formatted

--- digital_ocean/main/ssh_key.tf
+++ digital_ocean/main/ssh_key.tf
@@ -1,4 +1,4 @@
 resource "digitalocean_ssh_key" "default" {
-    name = "${var.ssh_key_name}"
-    public_key = "${file("${var.ssh_key_path}")}"
+  name       = "${var.ssh_key_name}"
+  public_key = "${file("${var.ssh_key_path}")}"
 }
