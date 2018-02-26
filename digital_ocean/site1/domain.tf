84d8d8d18da899767d4689333d3219bf30c38187
Clarence Mills <clarence.mills@gmail.com>
2018-02-26 11:27:25 -0500

    Variablized items within

--- digital_ocean/site1/domain.tf
+++ digital_ocean/site1/domain.tf
@@ -1,12 +1,20 @@
-resource "digitalocean_domain" "millsresidence" {
-  name       = "millsresidence.com"
+resource "digitalocean_domain" "domain_name" {
+  name       = "${var.web_domain}"
   ip_address = "${digitalocean_droplet.www1.ipv4_address}"
 }
 
-resource "digitalocean_record" "millsresidence" {
-  domain = "${digitalocean_domain.millsresidence.name}"
+resource "digitalocean_record" "domain_record" {
+  domain = "${digitalocean_domain.domain_name.name}"
+  type   = "A"
+  name   = "master"
+  ttl    = "${var.ttl_life}"
+  value  = "${digitalocean_droplet.www1.ipv4_address}"
+}
+
+resource "digitalocean_record" "domain_record2" {
+  domain = "${digitalocean_domain.domain_name.name}"
   type   = "A"
   name   = "www"
-  ttl    = "30"
+  ttl    = "${var.ttl_life}"
   value  = "${digitalocean_droplet.www1.ipv4_address}"
 }
