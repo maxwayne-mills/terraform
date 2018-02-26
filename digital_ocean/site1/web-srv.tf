1431c8909dfbc77e35083d03f4f1d4d07ed38ece
Clarence Mills <clarence.mills@gmail.com>
2018-02-26 00:29:26 -0500

    update to use web-srv

--- digital_ocean/site1/web-srv.tf
+++ digital_ocean/site1/web-srv.tf
@@ -6,7 +6,7 @@ resource "digitalocean_droplet" "www1" {
   ssh_keys = ["${var.ssh_fingerprint}"]
 
   provisioner "local-exec" {
-    command = "sleep 20 && echo \"[webserver1]\n${digitalocean_droplet.www1.ipv4_address} ansible_connection=ssh ansible_ssh_user=root\" > inventory && ansible-playbook -i inventory ansible/roles/websrv/websrv.yml"
+    command = "sleep 20 && echo \"[webserver1]\n${digitalocean_droplet.www1.ipv4_address} ansible_connection=ssh ansible_ssh_user=root\" > inventory && ansible-playbook -i inventory ansible/roles/websrv/websrv.yml -e domain=millsresidence.com"
   }
 }
 
