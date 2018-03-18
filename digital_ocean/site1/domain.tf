86bf3dca42318b8e9c7d9b773fc249231cd0c289
Clarence Mills <clarence.mills@gmail.com>
2018-03-18 19:42:08 -0400

    .

--- digital_ocean/site1/domain.tf
+++ digital_ocean/site1/domain.tf
@@ -1,18 +1,10 @@
-resource "digitalocean_domain" "domain_name" {
-  name       = "${var.web_domain}"
+resource "digitalocean_domain" "domain_name1" {
+  name       = "${var.web_domain1}"
   ip_address = "${digitalocean_droplet.www1.ipv4_address}"
 }
 
 resource "digitalocean_record" "domain_record1" {
-  domain = "${digitalocean_domain.domain_name.name}"
-  type   = "A"
-  name   = "master"
-  ttl    = "${var.ttl_life}"
-  value  = "${digitalocean_droplet.www1.ipv4_address}"
-}
-
-resource "digitalocean_record" "domain_record2" {
-  domain = "${digitalocean_domain.domain_name.name}"
+  domain = "${digitalocean_domain.domain_name1.name}"
   type   = "A"
   name   = "www"
   ttl    = "${var.ttl_life}"
@@ -25,7 +17,7 @@ resource "digitalocean_domain" "domain_name2" {
   ip_address = "${digitalocean_droplet.www1.ipv4_address}"
 }
 
-resource "digitalocean_record" "domain_record3" {
+resource "digitalocean_record" "domain_record2" {
   domain = "${digitalocean_domain.domain_name2.name}"
   type   = "A"
   name   = "www"
@@ -35,12 +27,12 @@ resource "digitalocean_record" "domain_record3" {
 
 # Third domain 
 resource "digitalocean_domain" "domain_name3" {
-  name       = "${var.web_domain2}"
+  name       = "${var.web_domain3}"
   ip_address = "${digitalocean_droplet.www1.ipv4_address}"
 }
 
-resource "digitalocean_record" "domain_record4" {
-  domain = "${digitalocean_domain.domain_name2.name}"
+resource "digitalocean_record" "domain_record3" {
+  domain = "${digitalocean_domain.domain_name3.name}"
   type   = "A"
   name   = "www"
   ttl    = "${var.ttl_life}"
