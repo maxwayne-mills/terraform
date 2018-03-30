030bee3501b8cd6b6b841b080d0010201b65f7ce
Clarence Mills <clarence.mills@gmail.com>
2018-03-30 12:52:23 -0400

    Update documentation

--- scaleway/provider.tf
+++ scaleway/provider.tf
@@ -1,9 +1,12 @@
 # You need to export the items below to authenticate to the Scaleway platform
+# export SCALEWAY_ORGANIZATION=""
 # export SCALEWAY_TOKEN=""
-# export SCALEWAY_TOKEN=""
-# If you create an infrastructure with a token and then delete the token without deleting the infratstructure
+# If you create an infrastructure with a token and then delete the token without deleting the infrastructure
 # you will have to delete the infrastructure manually.
 
-provider "scaleway" {
+# Don't include either the scaleway_organization or token within this file, 
+#read it from env variables. It will generate an error if you do.
+
+provider {
   region = "${var.region}"
 }
