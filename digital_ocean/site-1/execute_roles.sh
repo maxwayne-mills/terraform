0532a50f0560f1de6103a7994b3a3175839de662
Clarence Mills <clarence.mills@gmail.com>
2018-04-24 23:03:31 -0400

    update with new domain

--- digital_ocean/site-1/execute_roles.sh
+++ digital_ocean/site-1/execute_roles.sh
@@ -2,4 +2,4 @@
 
 play=$(which ansible-playbook)
 
-$play ansible/playbooks/publish_website.yml -e "server_name=srv1 domain_name=fixyourip.com repo_name=https://github.com/maxwayne-mills/fixyourip.com.git" -vvvv
+$play ansible/playbooks/publish_website.yml -e "server_name=srv1 domain_name=becomeonewiththecode.com repo_name=https://github.com/clarencemills/becomeonewiththecode.com.git" -vv
