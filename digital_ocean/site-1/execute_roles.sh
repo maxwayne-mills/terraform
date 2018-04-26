53218afef4af332a14662641a32204b9bc0ab3b7
Clarence Mills <cmills@opensitesolutions.com>
2018-04-26 13:55:05 -0400

    removed as this is present in shell_scripts repo

--- digital_ocean/site-1/execute_roles.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-play=$(which ansible-playbook)
-domain=$1
-git_repo=$2
-
-$play ansible/playbooks/publish_website.yml -e "server_name=srv1 domain_name=$domain repo_name=$git_repo" -vv
