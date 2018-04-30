3ffb1f02d51b7c6777a8f234344cb97e2a25ed5f
Clarence Mills <cmills@opensitesolutions.com>
2018-04-30 00:28:13 -0400

    Add firewall rules

---
+++ digital_ocean/dev/firewall.tf
@@ -0,0 +1,25 @@
+resource "digitalocean_firewall" "tst-srv" {
+  name        = "Access-to-Server"
+  droplet_ids = ["${digitalocean_droplet.tst-srv.id}"]
+
+  inbound_rule = [
+    {
+      protocol         = "tcp"
+      port_range       = "22"
+      source_addresses = ["0.0.0.0/0", "::/0"]
+    },
+  ]
+
+  outbound_rule = [
+    {
+      protocol              = "tcp"
+      port_range            = "1-65535"
+      destination_addresses = ["0.0.0.0/0", "::/0"]
+    },
+    {
+      protocol              = "udp"
+      port_range            = "1-65535"
+      destination_addresses = ["0.0.0.0/0", "::/0"]
+    },
+  ]
+}
