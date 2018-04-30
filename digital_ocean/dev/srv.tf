6cc9376e2f65cab1d18a0b75b74d854f4de36bbc
Clarence Mills <cmills@opensitesolutions.com>
2018-04-30 00:29:45 -0400

    provision server outside of teraform, add monitoring and resize parameters

--- digital_ocean/dev/srv.tf
+++ digital_ocean/dev/srv.tf
@@ -1,15 +1,17 @@
 resource "digitalocean_droplet" "tst-srv" {
-  image     = "${var.droplet_image}"
-  name      = "${var.tst-srv}"
-  region    = "${var.region}"
-  size      = "${var.droplet_memory_size}"
-  ssh_keys  = ["${var.ssh_fingerprint}"]
-  user_data = "{$file(cloud_init/user_data.yml)}"
+  image       = "${var.droplet_image}"
+  name        = "${var.tst-srv}"
+  region      = "${var.region}"
+  size        = "${var.droplet_memory_size}"
+  ssh_keys    = ["${var.ssh_fingerprint}"]
+  user_data   = "${file("cloud_init/user_data.yml")}"
+  monitoring  = "true"
+  resize_disk = "false"
 
   # Create ansible configuration file
-  provisioner "local-exec" {
-    command = "sleep 20 && echo \"[srv1]\n${digitalocean_droplet.tst-srv.ipv4_address} ansible_connection=ssh ansible_ssh_user=deployuser\" > inventory"
-  }
+  #  provisioner "local-exec" {
+  #command = "sleep 20 && echo \"[srv1]\n${digitalocean_droplet.tst-srv.ipv4_address} ansible_connection=ssh ansible_ssh_user=deployuser\" > inventory"
+  #}
 }
 
 resource "digitalocean_floating_ip" "tst-srv" {
